2769ef0b59ed1953521f3770eacc95b3dc8c5b75
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-04-17 03:13:35 +0900

    Refactoring as pointed out by Code Climate.

--- src/includes/favorite.ts
+++ src/includes/favorite.ts
@@ -1,27 +1,30 @@
-import * as path            from "path";
 import * as Installer       from "./installer";
 import { ExtensionSetting } from "./settings/extension";
 
-export async function randomSet() {
-	const setting = new ExtensionSetting();
-
-	if (
-		!(typeof(setting.isFavoriteExist) === "boolean") &&
-		setting.favoriteRandomSet
-	) {
-		const type      = { Image: 0, Slide: 1 };
-		const choice    = (min: number, max: number) => {
-			return Math.floor(Math.random() * (max - min + 1)) +min;
-		};
-		const installer = Installer.getInstance(setting);
-		let   favorites: {
-			 name: string, type: number
-		}[]             = [];
+const type           = { Image: 0, Slide: 1 };
+const choice         = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min + 1)) +min;
+};
+const setting        = new ExtensionSetting();
+const installer      = Installer.getInstance(setting);
+const favorite2array = (
+	(favoriteType: number, favorites: { [key: string]: any }) => {
+		let temporary: { name: string, type: number }[] = [];
+		Object.keys(favorites).map((name) => {temporary.push({ name: name, type: favoriteType});});
 		
-		Object.keys(setting.favoriteImageSet).map((name) => { favorites.push({ name: name, type: type.Image });});
-		Object.keys(setting.favoriteSlideSet).map((name) => { favorites.push({ name: name, type: type.Slide});});
+		return temporary;
+	}
+)
 
-		let   selection = favorites[choice(0, favorites.length -1)];
+export async function randomSet() {
+	if (!(typeof(setting.isFavoriteExist) === "boolean") && setting.favoriteRandomSet) {
+		let favorites: {
+			name: string,
+			type: number
+		}[]           = new Array()
+							.concat(favorite2array(type.Image, setting.favoriteImageSet))
+							.concat(favorite2array(type.Slide, setting.favoriteSlideSet));
+		let selection = favorites[choice(0, favorites.length -1)];
 
 		if (selection.type === type.Image) {
 			let favorite = setting.favoriteImageSet[selection.name];
