22ee992b7c7a2fbd1adb8e51adc00b4ee91928ad
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-04-22 21:33:37 +0900

    Refactoring as pointed out by Code Climate.

--- src/includes/constant.ts
+++ src/includes/constant.ts
@@ -1,19 +1,22 @@
 import { VSCodePreset } from "./utils/base/vscodePreset";
 
+const confirmItemsCreat = (description: { yes: string, no: string, return?: string }) => {
+	return [
+		VSCodePreset.create(VSCodePreset.Icons.check, "Yes", description.yes),
+		VSCodePreset.create(VSCodePreset.Icons.x,     "No",  description.no)
+	].concat(
+		description.return ? VSCodePreset.create(VSCodePreset.Icons.reply, "Return", description.return) : []
+	);
+}
+
 export const wallpaperType: { [key: string]: number } = { Image: 0, Slide: 1 };
 export const applyImageFile                           = ["png", "jpg", "jpeg", "gif"];
 export const maximumOpacity                           = 0.5;
 export const minimumOpacity                           = 1;
 export const minimumSlideInterval                     = 0.1;
 export const slideIntervalUnit                        = ["Hour", "Minute", "Second", "MilliSecond"].map((label) => ({ label }));
-export const slideRandomPlay                          = [
-	VSCodePreset.create(VSCodePreset.Icons.check,      "Yes",        "Random"),
-	VSCodePreset.create(VSCodePreset.Icons.x,          "No",         "Not random"),
-];
-export const slideEffectFadeIn                        = [
-	VSCodePreset.create(VSCodePreset.Icons.check,      "Yes",        "Fade in effect"),
-	VSCodePreset.create(VSCodePreset.Icons.x,          "No",         "Not effect"),
-];
+export const slideRandomPlay                          = confirmItemsCreat({ yes: "Random",          no: "Not random." });
+export const slideEffectFadeIn                        = confirmItemsCreat({ yes: "Fade in effect.", no: "Not effect." });
 export const favoriteProcess                          = [
 	VSCodePreset.create(VSCodePreset.Icons.repoPush,   "Register",   "Register the current settings to favorite."),
 	VSCodePreset.create(VSCodePreset.Icons.repoDelete, "UnRegister", "UnRegister favorite settings."),
@@ -21,8 +24,4 @@ export const favoriteProcess                          = [
 	VSCodePreset.create(VSCodePreset.Icons.merge,      "Start Up",   "Start up settings."),
 	VSCodePreset.create(VSCodePreset.Icons.reply,      "Return",     "Return without saving any changes."),
 ];
-export const favoriteRandomSet                        = [
-	VSCodePreset.create(VSCodePreset.Icons.check,      "Yes",        "Random wallpaper at start up."),
-	VSCodePreset.create(VSCodePreset.Icons.x,          "No",         "Not random."),
-	VSCodePreset.create(VSCodePreset.Icons.reply,      "Return",     "Return without saving any changes."),
-];
+export const favoriteRandomSet                        = confirmItemsCreat({ yes: "Random wallpaper at start up.", no: "Not random.", return: "Return without saving any changes." });
