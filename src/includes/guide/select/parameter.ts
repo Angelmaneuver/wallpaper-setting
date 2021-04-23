542c609073e3aa20e8b635e164b299f728ff2d60
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-04-23 22:28:19 +0900

    Refactoring as pointed out by Code Climate.

--- src/includes/guide/select/parameter.ts
+++ src/includes/guide/select/parameter.ts
@@ -1,5 +1,5 @@
 import { InputStep, MultiStepInput } from "../../utils/multiStepInput";
-import { BaseQuickPickGuide }        from "../base/pick";
+import { AbstractQuickPickGuide }    from "../base/pick";
 import { QuickPickItem }             from "vscode";
 import { State }                     from "../base/base";
 import { VSCodePreset }              from "../../utils/base/vscodePreset";
@@ -8,8 +8,8 @@ import * as Slide                    from "../slide";
 
 type optionState = { subState?: Partial<State>, initialValue?: any };
 
-export class SelectParameterType extends BaseQuickPickGuide {
-	private static templateItems: QuickPickItem[]                = [
+export class SelectParameterType extends AbstractQuickPickGuide {
+	private static templateItems: Array<QuickPickItem>     = [
 		VSCodePreset.create(VSCodePreset.Icons.fileMedia, "Image Path",            "Set the image to be used as the wallpaper."),
 		VSCodePreset.create(VSCodePreset.Icons.folder,    "Image Files Path",      "Set the images to be used as the slide."),
 		VSCodePreset.create(VSCodePreset.Icons.eye,       "Opacity",               "Set the opacity of the wallpaper."),
@@ -21,19 +21,21 @@ export class SelectParameterType extends BaseQuickPickGuide {
 		VSCodePreset.create(VSCodePreset.Icons.reply,     "Return",                "Return without saving any changes."),
 	];
 
-	private static item2GuideParameter: { [key: number]: any[] } = {};
+	private guideParameters: { [key: number]: Array<any> } = {};
 
-	constructor(
-		state: State,
-	) {
-		super(state);
+	public init(): void {
+		super.init();
 
-		this.placeholder = "Select the item you want to set.";
-
-		if (Object.keys(SelectParameterType.item2GuideParameter).length === 0) {
-			this.initial();
+		this.placeholder     = "Select the item you want to set.";
+		this.guideParameters = {
+			0: [this.settingItemId.filePath,          "ImageFilePathGuide",   " - Image Path",            this.createOptionState(this.settingItemId.filePath)],
+			1: [this.settingItemId.slideFilePaths,    "SlideFilePathsGuide",  " - Image Files Path",      this.createOptionState(this.settingItemId.slideFilePaths)],
+			2: [this.settingItemId.opacity,           "OpacityGuide",         " - Opacity",               this.createOptionState(this.settingItemId.opacity)],
+			3: [this.settingItemId.slideInterval,     "SlideIntervalGuide",   " - Slide Interval",        this.createOptionState(this.settingItemId.slideInterval)],
+			4: [this.settingItemId.slideIntervalUnit, "BaseQuickPickGuide",   " - Slide Interval Unit",   this.createOptionState(this.settingItemId.slideIntervalUnit)],
+			5: [this.settingItemId.slideRandomPlay,   "SlideRandomPlayGuide", " - Slide Random Playback", this.createOptionState(this.settingItemId.slideRandomPlay)],
+			6: [this.settingItemId.slideEffectFadeIn, "BaseQuickPickGuide",   " - Slide Effect Fade In",  this.createOptionState(this.settingItemId.slideEffectFadeIn)]
 		}
-
 	}
 
 	public async show(input: MultiStepInput): Promise<void | InputStep> {
@@ -45,39 +47,28 @@ export class SelectParameterType extends BaseQuickPickGuide {
 			SelectParameterType.templateItems[4],
 			SelectParameterType.templateItems[5],
 			SelectParameterType.templateItems[6]
-		].concat(
-			Object.keys(this.guideGroupResultSet).length > 0
-				? SelectParameterType.templateItems[7]
-				: []
+		].concat(Object.keys(this.guideGroupResultSet).length > 0 ? SelectParameterType.templateItems[7] : []
 		).concat(SelectParameterType.templateItems[8]);
 
 		await super.show(input);
 	}
 
-	public async after(): Promise<void> {
+	protected getExecute(): (() => Promise<void>) | undefined {
 		if (this.activeItem === SelectParameterType.templateItems[7]) {
-			await this.save();
+			return async () => { await this.save(); };
 		} else if (this.activeItem === SelectParameterType.templateItems[8]) {
-			this.prev();
+			return async () => { this.prev(); };
 		} else if (this.activeItem) {
-			Reflect.apply(
-				this.createNextStep,
-				this,
-				SelectParameterType.item2GuideParameter[SelectParameterType.templateItems.indexOf(this.activeItem)]
-			);
+			return async () => {
+				if (this.activeItem) {
+					Reflect.apply(
+						this.createNextStep,
+						this,
+						this.guideParameters[SelectParameterType.templateItems.indexOf(this.activeItem)]
+					);
+				}
+			};
 		}
-	}
-
-	private initial(): void {
-		SelectParameterType.item2GuideParameter = {
-			0: [this.settingItemId.filePath,          "ImageFilePathGuide",   " - Image Path",            this.createOptionState(this.settingItemId.filePath)],
-			1: [this.settingItemId.slideFilePaths,    "SlideFilePathsGuide",  " - Image Files Path",      this.createOptionState(this.settingItemId.slideFilePaths)],
-			2: [this.settingItemId.opacity,           "OpacityGuide",         " - Opacity",               this.createOptionState(this.settingItemId.opacity)],
-			3: [this.settingItemId.slideInterval,     "SlideIntervalGuide",   " - Slide Interval",        this.createOptionState(this.settingItemId.slideInterval)],
-			4: [this.settingItemId.slideIntervalUnit, "BaseQuickPickGuide",   " - Slide Interval Unit",   this.createOptionState(this.settingItemId.slideIntervalUnit)],
-			5: [this.settingItemId.slideRandomPlay,   "SlideRandomPlayGuide", " - Slide Random Playback", this.createOptionState(this.settingItemId.slideRandomPlay)],
-			6: [this.settingItemId.slideEffectFadeIn, "BaseQuickPickGuide",   " - Slide Effect Fade In",  this.createOptionState(this.settingItemId.slideEffectFadeIn)]
-		};
 	}
 
 	private createOptionState(itemId: string): optionState {
@@ -105,7 +96,7 @@ export class SelectParameterType extends BaseQuickPickGuide {
 		additionalTitle: string,
 		optionState?:    optionState
 	): void {
-		let state = this.createState(additionalTitle, this.guideGroupId, 0, itemId);
+		let state = this.createBaseState(additionalTitle, this.guideGroupId, 0, itemId);
 
 		if (optionState) {
 			if (optionState.subState) {
