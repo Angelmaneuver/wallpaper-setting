69f4134395d0942a25ac7d377e2af1ab936bb6a9
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-05-01 15:35:05 +0900

    Added test code.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,8 +1,32 @@
-import * as assert from "assert";
+import * as assert     from "assert";
+import * as vscode     from "vscode";
+import * as testTarget from "../../extension";
 
-suite('Extension Test Suite', () => {
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+suite('Extension Test Suite', async () => {
+	const extensionId       = 'angelmaneuver.wallpaper-setting';
+	const extenisonCommands = ['wallpaper-setting.guidance'];
+
+	test('Present', () => {
+		assert.ok(vscode.extensions.getExtension(extensionId))
+	});
+
+    test('Activate', async () => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		await vscode.extensions.getExtension(extensionId)?.activate().then((api) => { assert.ok(true); });
+	});
+
+	test('Register Commands', async () => {
+		return vscode.commands.getCommands(true).then((commands) => {
+			for (const extensionCommand of extenisonCommands) {
+				if (!commands.includes(extensionCommand)) {
+					assert.fail();
+				}
+			}
+		})
+	});
+
+    test('DeActivate', async () => {
+		testTarget.deactivate();
+		assert.ok(true);
 	});
 });
