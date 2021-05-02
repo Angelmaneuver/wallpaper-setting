c3204a784c8a00c7f9a0e45efcec34e988fd18eb
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2021-05-02 21:20:53 +0900

    Added test code.

---
+++ src/test/suite/includes/guidance.test.ts
@@ -0,0 +1,47 @@
+import * as sinon           from "sinon";
+import * as vscode          from "vscode";
+import * as testTarget      from "../../../includes/guidance";
+import { MultiStepInput }   from "../../../includes/utils/multiStepInput";
+import { State }            from "../../../includes/guide/base/base";
+import { GuideFactory }     from "../../../includes/guide/factory/base";
+import { StartMenuGuide }   from "../../../includes/guide/begin";
+
+suite('Guidance Test Suite', async () => {
+	test('guidance', async () => {
+		const multiStepInputStub = sinon.stub(MultiStepInput, "run");
+		const guideFactoryStub   = sinon.stub(GuideFactory,   "create");
+		const windowMock         = sinon.mock(vscode.window);
+		const commandMock        = sinon.mock(vscode.commands);
+		const context            = {} as vscode.ExtensionContext;
+
+		multiStepInputStub.onFirstCall().throws(new Error("Stub Error"));
+		windowMock.expects("showWarningMessage").withArgs("Stub Error").once();
+		await testTarget.guidance(context);
+
+		guideFactoryStub.onSecondCall().callsFake(
+			(className: string, state: State, context: vscode.ExtensionContext) => {
+				state.message = "Stub Info";
+				return new StartMenuGuide(state, context);
+			}
+		);
+		windowMock.expects("showInformationMessage").withArgs("Stub Info").once();
+		await testTarget.guidance(context);
+
+		guideFactoryStub.onThirdCall().callsFake(
+			(className: string, state: State, context: vscode.ExtensionContext) => {
+				state.reload = true;
+				return new StartMenuGuide(state, context);
+			}
+		);
+		commandMock.expects("executeCommand").once().withArgs("workbench.action.reloadWindow");
+		await testTarget.guidance(context);
+
+		windowMock.verify();
+		commandMock.verify();
+
+		guideFactoryStub.restore();
+		multiStepInputStub.restore();
+		windowMock.restore();
+		commandMock.restore();
+	});
+});
